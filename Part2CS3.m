e9169829f4b3985e4ab390f5d1d3ae4ac2f479bf
RRewey <112431383+RRewey@users.noreply.github.com>
2023-11-15 17:06:40 -0600

    Add files via upload

--- Part2CS3.m
+++ Part2CS3.m
@@ -1,1 +1,11 @@
 %%%Part2 CS3%%%
+
+load("lightField.mat");
+
+rays_x = rays(1,:);
+rays_y= rays(3,:);
+width = 0.03;
+Npixels = 900;
+
+[img,x,y] = rays2img(rays_x,rays_y,width,Npixels);
+imshow(img);
